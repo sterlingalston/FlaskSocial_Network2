d18689699257dbf9f84c51899ee600701439a6c8
Matthew Alston <sterlingalston@gmail.com>
2019-01-12 21:29:01 +0000

    added abort 404 without custom message

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask import (Flask, g, render_template, flash, redirect, url_for,
+                  abort)
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, logout_user,
                              login_required, current_user)
@@ -100,11 +101,17 @@ def index():
   
 @app.route('/stream')
 @app.route('/stream/<username>')
+@login_required
 def stream(username = None):
     template = 'stream.html'
     if username and username != current_user.username:
-        user = models.User.select().where(models.User.username**username).get() #** is the 'like' operator
-        stream = user.posts.limit(100)
+          try:
+            user = models.User.select().where(
+                models.User.username**username).get() #** is the 'like' operator
+          except models.DoesNotExist:
+                abort(404)
+          else:
+            stream = user.posts.limit(100)
     else:
         stream = current_user.get_stream().limit(100)
         user = current_user
@@ -118,7 +125,7 @@ def follow(username):
     try:
         to_user = models.User.get(models.User.username**username) # case insensitve search ** is 'like'
     except models.DoesNotExist:
-        pass
+        abort(404)
     else:
         try:
             models.Relationship.create(
@@ -134,6 +141,8 @@ def follow(username):
 @app.route('/post/<int:post_id>')
 def view_post(post_id):
     posts = models.Post.select().where(models.Post.id == post_id)
+    if posts.count() == 0:
+        abort(404)
     return render_template('stream.html', stream = posts)
 
 @app.route('/unfollow/<username>')
@@ -142,7 +151,7 @@ def unfollow(username):
     try:
         to_user = models.User.get(models.User.username**username) # case insensitve search ** is 'like'
     except models.DoesNotExist:
-        pass
+        abort(404)
     else:
         try:
             models.Relationship.get(
